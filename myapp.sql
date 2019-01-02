dd092338e4b7ce7a294a211ca32202f0226f031d
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 08:47:21 +0900

    #23 サブクエリを使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -12,7 +12,10 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
+drop table if exists users_with_team;
+create table users_with_team as
 select
+  id,
   name,
   score,
   case
@@ -20,5 +23,6 @@ select
     when score > 6.0 then 'Team-B'
     else 'Team-C'
   end as team
-from
-  users;
+from users;
+
+select sum(score), team from users_with_team group by team having sum(score) >= 10.0;
