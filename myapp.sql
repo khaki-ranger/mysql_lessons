508f6c4dd604c988d7cb6c4d0033fea9f6e4eb8c
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 06:53:42 +0900

    #09 フィールドに制限をかけてみよう

--- myapp.sql
+++ myapp.sql
@@ -1,19 +1,14 @@
 drop table if exists users;
 create table users (
-  id int unsigned,
-  name varchar(20),
-  score float
+  id int unsigned primary key auto_increment,
+  name varchar(20) unique,
+  score float default 0.0
 );
+desc users;
 
--- insert into users (id, name, score) values (1, 'Yohei', 5.8);
--- insert into users (id, name, score) values (2, 'fkoji', 8.2);
--- insert into users (id, name, score) values (3, 'dotinstall', 6.1);
--- insert into users (id, name, score) values (4, 'yamada', null);
-
-insert into users (id, name, score) values
-  (1, 'Yohei', 5.8),
-  (2, 'fkoji', 8.2),
-  (3, 'dotinstall', 6.1),
-  (4, 'yamada', null);
+insert into users (name, score) values ('Yohei', 5.8);
+insert into users (name, score) values ('fkoji', 8.2);
+insert into users (name, score) values ('dotinstall', 6.1);
+insert into users (name, score) values ('yamada', null);
 
 select * from users;
