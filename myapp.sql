f44bb38f89e942b707aa05c51a25d19627c48ea0
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 07:29:57 +0900

    #11 レコードを抽出してみよう

--- myapp.sql
+++ myapp.sql
@@ -4,11 +4,16 @@ create table users (
   name varchar(20) unique,
   score float default 0.0
 );
-desc users;
 
-insert into users (name, score) values ('Yohei', 5.8);
+insert into users (name, score) values ('taguchi', 5.8);
 insert into users (name, score) values ('fkoji', 8.2);
 insert into users (name, score) values ('dotinstall', 6.1);
+insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
+insert into users (name, score) values ('tashiro', 7.9);
 
-select * from users;
+-- select id, name from users;
+-- select * from users where score >= 3.0 and score <= 6.0;
+-- select * from users where score between 3.0 and 6.0;
+select * from users where name = 'taguchi' or name = 'fkoji';
+select * from users where name in ('taguchi', 'fkoji');
