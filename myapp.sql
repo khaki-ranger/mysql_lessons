259d832f5ecb132bc4af08b63ccf3ae93eb569c1
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 05:52:01 +0900

    #08 レコードを挿入してみよう

--- myapp.sql
+++ myapp.sql
@@ -4,3 +4,16 @@ create table users (
   name varchar(20),
   score float
 );
+
+-- insert into users (id, name, score) values (1, 'Yohei', 5.8);
+-- insert into users (id, name, score) values (2, 'fkoji', 8.2);
+-- insert into users (id, name, score) values (3, 'dotinstall', 6.1);
+-- insert into users (id, name, score) values (4, 'yamada', null);
+
+insert into users (id, name, score) values
+  (1, 'Yohei', 5.8),
+  (2, 'fkoji', 8.2),
+  (3, 'dotinstall', 6.1),
+  (4, 'yamada', null);
+
+select * from users;
