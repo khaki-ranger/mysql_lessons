33cf0433cad0157dd77b0ec79bb907badb6d991a
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 08:03:48 +0900

    #17 enum型を使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -2,18 +2,12 @@ drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
   name varchar(20) unique,
-  score float default 0.0
+  score float default 0.0,
+  rank enum('gold', 'silver', 'bronze')
 );
 
-insert into users (name, score) values ('taguchi', 5.8);
-insert into users (name, score) values ('fkoji', 8.2);
-insert into users (name, score) values ('dotinstall', 6.1);
-insert into users (name, score) values ('Tanaka', 4.2);
-insert into users (name, score) values ('yamada', null);
-insert into users (name, score) values ('tashiro', 7.9);
+insert into users (name, score, rank) values ('taguchi', 5.8, 'silver');
+insert into users (name, score, rank) values ('fkoji', 8.2, 'gold');
+insert into users (name, score, rank) values ('dotinstall', 6.1, 'red');
 
--- select * from users order by score;
--- select * from users where score is not null order by score desc;
--- select * from users limit 3;
--- select * from users limit 3 offset 3;
-select * from users order by score desc limit 3;
+select * from users where rank = 2;
