def142d75875082d7fd57eddf7180973bcd1bebb
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 08:24:23 +0900

    #19 if､caseを使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -1,13 +1,24 @@
 drop table if exists users;
 create table users (
   id int unsigned primary key auto_increment,
-  name varchar(20) unique,
-  score float default 0.0,
-  rank enum('gold', 'silver', 'bronze')
+  name varchar(20),
+  score float
 );
 
-insert into users (name, score, rank) values ('taguchi', 5.8, 'silver');
-insert into users (name, score, rank) values ('fkoji', 8.2, 'gold');
-insert into users (name, score, rank) values ('dotinstall', 6.1, 'red');
+insert into users (name, score) values ('taguchi', 5.8);
+insert into users (name, score) values ('fkoji', 8.2);
+insert into users (name, score) values ('dotinstall', 6.1);
+insert into users (name, score) values ('Tanaka', 4.2);
+insert into users (name, score) values ('yamada', null);
+insert into users (name, score) values ('tashiro', 7.9);
 
-select * from users where rank = 2;
+select
+  name,
+  score,
+  case
+    when score > 8.0 then 'Team-A'
+    when score > 6.0 then 'Team-B'
+    else 'Team-C'
+  end as team
+from
+  users;
