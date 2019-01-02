057aa694f494872e124e0b3dcc88d8e93f47766b
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 07:35:34 +0900

    #12 文字列を抽出条件にしてみよう

--- myapp.sql
+++ myapp.sql
@@ -12,8 +12,7 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
--- select id, name from users;
--- select * from users where score >= 3.0 and score <= 6.0;
--- select * from users where score between 3.0 and 6.0;
-select * from users where name = 'taguchi' or name = 'fkoji';
-select * from users where name in ('taguchi', 'fkoji');
+-- select * from users where name like 't%';
+-- select * from users where name like binary 'T%';
+-- select * from users where name like '______';
+select * from users where name like '_a%';
