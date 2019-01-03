e8d8b3102fdd037fe1de5a2583bc4bcf4444d72f
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 12:32:37 +0900

    完了

--- myapp.sql
+++ myapp.sql
@@ -2,32 +2,20 @@ drop table if exists posts;
 create table posts (
   id int unsigned primary key auto_increment,
   title varchar(255),
-  body text
+  body text,
+  created datetime default current_timestamp,
+  updated datetime default current_timestamp on update current_timestamp
 );
-drop table if exists logs;
-create table logs (
-  id int unsigned primary key auto_increment,
-  msg varchar(255)
-);
-
-drop trigger if exists posts_update_trigger;
-delimiter //
-create trigger posts_update_trigger after update on posts for each row
-  begin
-    insert into logs (msg) values ('post updated!');
-    insert into logs (msg) values (concat(old.title, ' -> ', new.title));
-  end;
-//
-delimiter ;
 
 insert into posts (title, body) values ('title 1', 'body 1');
 insert into posts (title, body) values ('title 2', 'body 2');
 insert into posts (title, body) values ('title 3', 'body 3');
 
-update posts set title = 'title 2 updated!' where id = 2;
+update posts set created = '2016-12-31 10:00:00' where id = 2;
 
-select * from posts;
-select * from logs;
+-- select created, date_add(created, interval 14 day) from posts;
+-- select created, date_add(created, interval 2 week) from posts;
+select created, date_format(created, '%W %M %Y') from posts;
 
 
 
