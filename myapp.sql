8774484d83a90ba69c0cd9560426c3af2f27c4ab
khaki-ranger <yamanogomora@gmail.com>
2019-01-03 09:31:57 +0900

    #25 トランザクションを使ってみよう

--- myapp.sql
+++ myapp.sql
@@ -12,17 +12,9 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
-drop table if exists users_with_team;
-create table users_with_team as
-select
-  id,
-  name,
-  score,
-  case
-    when score > 8.0 then 'Team-A'
-    when score > 6.0 then 'Team-B'
-    else 'Team-C'
-  end as team
-from users;
+start transaction;
+update users set score = score - 1.2 where name = 'fkoji';
+update users set score = score + 1.2 where name = 'taguchi';
+rollback;
 
-select sum(score), team from users_with_team group by team having sum(score) >= 10.0;
+select * from users;
